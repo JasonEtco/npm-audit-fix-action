6d6b5aa084e9be4f484339614ebe5bce67dd71cc
Jason Etcovitch <jasonetco@github.com>
2019-02-03 09:17:53 -0500

    Update actions-toolkit usage

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
+const Toolkit = require('actions-toolkit')``
 const runAudit = require('./lib/run-audit')
 const runAuditFix = require('./lib/run-audit-fix')
 const createPR = require('./lib/create-pr')
-const Toolkit = require('actions-toolkit')
 
 const tools = new Toolkit()
 
 runAudit(tools)
-.then(async ({ vulnerabilities, numVulnerabilities }) => {
-  if (numVulnerabilities === 0) {
-    console.log('No vulnerabilities found!')
-    return
-  }
+  .then(async ({ vulnerabilities, numVulnerabilities }) => {
+    if (numVulnerabilities === 0) {
+      console.log('No vulnerabilities found!')
+      return
+    }
 
-  const fixResult = await runAuditFix(tools)
-  console.log(fixResult)
+    const fixResult = await runAuditFix(tools)
+    console.log(fixResult)
 
-  return createPR({
-    vulnerabilities,
-    numVulnerabilities,
-    tools
+    return createPR({
+      vulnerabilities,
+      numVulnerabilities,
+      tools
+    })
   })
-})
-.catch(err => {
-  console.error(err)
-  process.exit(1)
-})
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
